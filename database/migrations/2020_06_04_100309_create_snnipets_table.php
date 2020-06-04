95ddd2f9846e6118cafd3f1fc43980cb674123f2
Carlos Revilla <clro26.k@gmail.com>
2020-06-04 12:38:03 +0200

    added model snnipets, controller and migration

---
+++ database/migrations/2020_06_04_100309_create_snnipets_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSnnipetsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('snnipets', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('code_snnipets');
+            $table->string('extension');
+            $table->integer('commentable_id');
+            $table->string('commentable_type');
+            $table->integer('user_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('snnipets');
+    }
+}
