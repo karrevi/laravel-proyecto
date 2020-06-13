839511cb9eded99c919d38bd23f41830897d04ac
Carlos Revilla <clro26.k@gmail.com>
2020-06-13 14:25:06 +0200

    changed guarded to fillable

--- app/Snnipets.php
+++ app/Snnipets.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Snnipets extends Model
 {
-    protected $guarded = [];
+    protected $fillable = [
+        'name', 'code_snnipets', 'extension', 'user_id'
+    ];
     public function user()
     {
         return $this->belongsTo('\App\User');
